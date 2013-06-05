a7f7708d42f6a744b8380baa69df50c6b9b78415
Jamie Hankins <jamiehankin@gmail.com>
2013-06-05 19:47:56 +0100

    Add my code

---
+++ common.php
@@ -0,0 +1,217 @@
+<?php 
+
+    // These variables define the connection information for your MySQL database 
+    $username = "root"; 
+    $password = ""; 
+    $host = "localhost"; 
+    $dbname = "whiteboard"; 
+
+    $GLOBALS['debug'] = true;
+
+    //Use UTF8
+    function dieError($exception)
+    {
+        if ($GLOBALS['debug'] == true)
+        {
+            die("Failed to run query: " . $exception->getMessage()); 
+        }
+        else
+        {
+            die("Database issues, Issue has been reported to the admins");
+        }
+    }
+
+    function tryLogin($username, $password)
+    {
+        $query = " 
+            SELECT 
+                id, 
+                username, 
+                password, 
+                salt, 
+                email 
+            FROM users 
+            WHERE 
+                username = :username 
+        "; 
+         
+        // The parameter values 
+        $query_params = array( 
+            ':username' => $username
+        ); 
+         
+        try 
+        { 
+            // Execute the query against the database 
+            $stmt = $db->prepare($query); 
+            $result = $stmt->execute($query_params); 
+        } 
+        catch(PDOException $ex) 
+        { 
+            dieError($ex);
+        } 
+         
+        // Retrieve the user data from the database.  If $row is false, then the username 
+        // they entered is not registered. 
+        $row = $stmt->fetch(); 
+        if($row) 
+        {              
+            if(genPassWithSalt($POST['password'], $row['salt']) === $row['password']) 
+            { 
+                return true;    
+            } 
+        } 
+    }
+
+    function genPassWithSalt($pass, $salt)
+    {
+        $check_password = hash('sha256', $pass . $salt); 
+        for($round = 0; $round < 65536; $round++) 
+        { 
+            $check_password = hash('sha256', $check_password . $salt); 
+        } 
+        return $check_password;
+    }
+
+    function genPass($pass)
+    {
+        // A salt is randomly generated here to protect again brute force attacks 
+        // and rainbow table attacks.
+        $salt = dechex(mt_rand(0, 2147483647)) . dechex(mt_rand(0, 2147483647));
+        // This hashes the password with the salt so that it can be stored securely 
+        // in your database.  The output of this next statement is a 64 byte hex 
+        // string representing the 32 byte sha256 hash of the password.  The original 
+        // password cannot be recovered from the hash.
+        $password = hash('sha256', $pass . $salt); 
+
+        // Next we hash the hash value 65536 more times.  The purpose of this is to 
+        // protect against brute force attacks.  Now an attacker must compute the hash 65537 
+        // times for each guess they make against a password, whereas if the password 
+        // were hashed only once the attacker would have been able to make 65537 different  
+        // guesses in the same amount of time instead of only one. 
+        for($round = 0; $round < 65536; $round++) 
+        { 
+            $password = hash('sha256', $password . $salt); 
+        } 
+
+        $returnArray['salt'] = $salt;
+        $returnArray['password'] = $password;
+        return $returnArray;
+    }
+
+    function fileInfo ($filepath)
+    {
+        $filearray = pathinfo($filepath);
+        return $filearray;
+        
+    }
+    function editPassword($email, $password)
+    {        
+    $stmt = $db->prepare("UPDATE users SET password=:pass WHERE email = :email");
+
+    $stmt->bindParam(':email', $email, PDO::PARAM_STR, 300);
+    $stmt->bindParam(':pass', $password, PDO::PARAM_STR, 300);
+
+    $stmt->execute();
+    }
+
+    function editEmail($email, $newemail, $userid)
+    {
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+        { 
+            die("Invalid E-Mail Address"); 
+        } 
+
+        $query = " 
+            SELECT 
+                1 
+            FROM users 
+            WHERE 
+                email = :email 
+        "; 
+         
+        // Define our query parameter values 
+        $query_params = array( 
+            ':email' => $email
+        ); 
+         
+        try 
+        { 
+            // Execute the query 
+            $stmt = $db->prepare($query); 
+            $result = $stmt->execute($query_params); 
+        } 
+
+        catch(PDOException $ex) 
+        { 
+            dieError($ex);
+        } 
+         
+        // Retrieve results (if any) 
+        $row = $stmt->fetch(); 
+        if($row) 
+        { 
+            die("This E-Mail address is already in use"); 
+        }
+        else
+        {
+            return $email;
+        }
+    }
+
+    //End of Funcs
+
+    $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'); 
+    
+    try 
+    {
+        //Try open the connection
+        $db = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $username, $password, $options); 
+    } 
+    catch(PDOException $ex) 
+    { 
+        dieError($ex);
+    } 
+     
+    //Set Errormode
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+     
+    // This statement configures PDO to return database rows from your database using an associative 
+    // array.  This means the array will have string indexes, where the string value 
+    // represents the name of the column in your database. 
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); 
+     
+    // This block of code is used to undo magic quotes.  Magic quotes are a terrible 
+    // feature that was removed from PHP as of PHP 5.4.  However, older installations 
+    // of PHP may still have magic quotes enabled and this code is necessary to 
+    // prevent them from causing problems.  For more information on magic quotes: 
+    // http://php.net/manual/en/security.magicquotes.php 
+    if(function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc()) 
+    { 
+        function undo_magic_quotes_gpc(&$array) 
+        { 
+            foreach($array as &$value) 
+            { 
+                if(is_array($value)) 
+                { 
+                    undo_magic_quotes_gpc($value); 
+                } 
+                else 
+                { 
+                    $value = stripslashes($value); 
+                } 
+            } 
+        } 
+     
+        undo_magic_quotes_gpc($_POST); 
+        undo_magic_quotes_gpc($_GET); 
+        undo_magic_quotes_gpc($_COOKIE); 
+    } 
+     
+    // This tells the web browser that your content is encoded using UTF-8 
+    header('Content-Type: text/html; charset=utf-8'); 
+     
+    // This initializes a session.
+    session_start(); 
+
+    //Don't close this else there will be redirection issues
---
+++ edit_account.php
@@ -0,0 +1,53 @@
+<?php 
+    require("common.php"); 
+     
+    // At the top of the page we check to see whether the user is logged in or not 
+    if(empty($_SESSION['user'])) 
+    { 
+        // If they are not, we redirect them to the login page. 
+        header("Location: login.php"); 
+        die("Redirecting to login.php"); 
+    } 
+     
+    // This if statement checks to determine whether the edit form has been submitted 
+    // If it has, then the account updating code is run, otherwise the form is displayed 
+    if(!empty($_POST)) 
+    { 
+        if($_POST['email'] != $_SESSION['user']['email']) 
+        { 
+            $newemail = editEmail($_SESSION['user']['email'], $_POST['email']);
+        } 
+
+        if(!empty($_POST['password'])) 
+        { 
+            $password = genPass($_POST['password']);
+        }
+         
+        // Now that the user's E-Mail address has changed, the data stored in the $_SESSION 
+        // array is stale; we need to update it so that it is accurate. 
+        $_SESSION['user']['email'] = $newemail; 
+         
+        // This redirects the user back to the members-only page after they register 
+        header("Location: private.php"); 
+         
+        // Calling die or exit after performing a redirect using the header function 
+        // is critical.  The rest of your PHP script will continue to execute and 
+        // will be sent to the user if you do not die or exit. 
+        die("Redirecting to private.php"); 
+    } 
+     
+?> 
+<h1>Edit Account</h1> 
+<form action="edit_account.php" method="post"> 
+    Username:<br /> 
+    <b><?php echo htmlentities($_SESSION['user']['username'], ENT_QUOTES, 'UTF-8'); ?></b> 
+    <br /><br /> 
+    E-Mail Address:<br /> 
+    <input type="text" name="email" value="<?php echo htmlentities($_SESSION['user']['email'], ENT_QUOTES, 'UTF-8'); ?>" /> 
+    <br /><br /> 
+    Password:<br /> 
+    <input type="password" name="password" value="" /><br /> 
+    <i>(leave blank if you do not want to change your password)</i> 
+    <br /><br /> 
+    <input type="submit" value="Update Account" /> 
+</form>
