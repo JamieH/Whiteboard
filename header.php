4f0778a8adc7230c562874499070fbd70edeffdc
Jamie Hankins <jamiehankin@gmail.com>
2013-06-06 21:20:21 +0100

    fix links

--- header.php
+++ header.php
@@ -2,7 +2,6 @@
 		<li><a href="https://docs.google.com/spreadsheet/ccc?key=0Ar405A90uLjQdHpBU210ZmVoNXVZZ1lMZzZtYmg2NkE#gid=18">Tracking Sheet</a></li>
 		<li><a href="https://adfs.strode-college.ac.uk/CookieAuth.dll?GetLogon?curl=Z2FadfsZ2FlsZ2FZ3FwaZ3Dwsignin1.0Z26wtrealmZ3DurnZ3AfederationZ3AMicrosoftOnlineZ26wctxZ3DwaZ253Dwsignin1.0Z2526rpsnvZ253D2Z2526ctZ253D1370532248Z2526rverZ253D6.1.6206.0Z2526wpZ253DMBI_KEYZ2526wreplyZ253DhttpsZ3AZ25252FZ25252Famxprd0112.outlook.comZ25252FowaZ25252FZ2526idZ253D260563Z2526whrZ253Dstudent.strode-college.ac.ukZ2526CBCXTZ253Dout&reason=0&formdir=6">Email</a></li>
 		<li><a href="http://elib.strode-college.ac.uk">Other Links</a></li>
-		<li><a href="edit_account.php">Change Password</a></li>
-		<li><a href="Member_Profile.php?ref=home">My Profile</a></li>
+		<li><a href="profile.php">My Profile</a></li>
 		<li><a href="logout.php">Log-out</a></li>
 	</div>
