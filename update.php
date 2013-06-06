a3039c5bd64e0805656115220b9de65e4149bb3e
Jamie Hankins <jamiehankin@gmail.com>
2013-06-06 17:57:57 +0100

    Update update script

--- update.php
+++ update.php
@@ -14,9 +14,6 @@
 		'git reset --hard HEAD',
 		'git pull origin master',
 		'git status',
-		'git submodule sync',
-		'git submodule update',
-		'git submodule status',
 	);
 
 	// Run the commands for output
