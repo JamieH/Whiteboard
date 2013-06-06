6a5fe80777d5c4764ab3a624105be6f23e0e562b
Matthew James <Quacky2200@hotmail.com>
2013-06-06 21:52:28 +0100

    fixed profile

--- course_view.php
+++ course_view.php
@@ -18,14 +18,9 @@ include 'common.php';
 <script src="JS/functions.js"></script>
 </head>
 <body onload="loadafterlogin()" onresize="IDPanelPlacement()">
-	<div id="IDPanel"> <?php echo ucfirst(strtolower(htmlentities($_SESSION['user']['username'], ENT_QUOTES, "UTF-8"))); ?> 
-		<a href="https://docs.google.com/spreadsheet/ccc?key=0Ar405A90uLjQdHpBU210ZmVoNXVZZ1lMZzZtYmg2NkE#gid=18"><li>Tracking Sheet</li></a>
-		<a href="https://adfs.strode-college.ac.uk/CookieAuth.dll?GetLogon?curl=Z2FadfsZ2FlsZ2FZ3FwaZ3Dwsignin1.0Z26wtrealmZ3DurnZ3AfederationZ3AMicrosoftOnlineZ26wctxZ3DwaZ253Dwsignin1.0Z2526rpsnvZ253D2Z2526ctZ253D1370532248Z2526rverZ253D6.1.6206.0Z2526wpZ253DMBI_KEYZ2526wreplyZ253DhttpsZ3AZ25252FZ25252Famxprd0112.outlook.comZ25252FowaZ25252FZ2526idZ253D260563Z2526whrZ253Dstudent.strode-college.ac.ukZ2526CBCXTZ253Dout&reason=0&formdir=6"><li>Email</li></a>
-		<a href="http://elib.strode-college.ac.uk"><li>Other Links</li></a>
-		<a href="Member_Profile.php?ref=cpassword"><li>Change Password</li></a>
-		<a href="Member_Profile.php?ref=home"><li>My Profile</li></a>
-		<a href="log-out.php"><li>Log-out</li></a>
-	</div>
+	<?php 
+        include('header.php');
+    ?>
 <div id="content">
 	<h2 align="center"> IT Extended Diploma Level 3</h2>
 	<div id="Unit 2 - Computer Systems" score="U">
