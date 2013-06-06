7c1446c79f7fa07ded6f2981598d2a45a42f8ce8
Matthew James <Quacky2200@hotmail.com>
2013-06-07 00:31:38 +0100

    git log

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <?php
 echo '<p>&copy;2013 All Rights Reserved - Thanks to <a href="http://rethinkvps.com">ReThinkVPS</a> for hosting us<br /><em>';
 echo shell_exec("git log -2 --pretty=format:'%h - %s (%ci)' --abbrev-commit");
+echo shell_exec("git log -4 --pretty=oneline");
 echo'</em></p>';
 ?>
