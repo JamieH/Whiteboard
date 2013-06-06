052c45f57e2baecb7ee171167c8c0f1e95a7c5d6
Jamie Hankins <jamiehankin@gmail.com>
2013-06-06 19:23:25 +0100

    fixed links added include header.php

--- course.php
+++ course.php
@@ -47,14 +47,7 @@ function expand(id, operation){
 </script>
 </head>
 <body onload="load()">
-	<div id="IDPanel"> <?php echo ucfirst(strtolower(htmlentities($_SESSION['user']['username'], ENT_QUOTES, "UTF-8"))); ?> 
-		<li><a href="https://docs.google.com/spreadsheet/ccc?key=0Ar405A90uLjQdHpBU210ZmVoNXVZZ1lMZzZtYmg2NkE#gid=18">Tracking Sheet</a></li>
-		<li><a href="https://adfs.strode-college.ac.uk/CookieAuth.dll?GetLogon?curl=Z2FadfsZ2FlsZ2FZ3FwaZ3Dwsignin1.0Z26wtrealmZ3DurnZ3AfederationZ3AMicrosoftOnlineZ26wctxZ3DwaZ253Dwsignin1.0Z2526rpsnvZ253D2Z2526ctZ253D1370532248Z2526rverZ253D6.1.6206.0Z2526wpZ253DMBI_KEYZ2526wreplyZ253DhttpsZ3AZ25252FZ25252Famxprd0112.outlook.comZ25252FowaZ25252FZ2526idZ253D260563Z2526whrZ253Dstudent.strode-college.ac.ukZ2526CBCXTZ253Dout&reason=0&formdir=6">Email</a></li>
-		<li><a href="http://elib.strode-college.ac.uk">Other Links</a></li>
-		<li><a href="Member_Profile.php?ref=cpassword">Change Password</a></li>
-		<li><a href="Member_Profile.php?ref=home">My Profile</a></li>
-		<li><a href="log-out.php">Log-out</a></li>
-	</div>
+<?php include'header.php';?>
 <div id="content">
 	<h2 align="center"> IT Extended Diploma Level 3</h2>
 	<div class="expand" id="Unit 2 - Computer Systems" score="U">
