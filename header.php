c23e5679588a86329f5cea63ee69ac84b0c3b0f9
Matthew James <Quacky2200@hotmail.com>
2013-06-06 20:23:05 +0100

    IDPanel should be fixed
    detects where top of the content is and coppies the top location to the
    IDPanel

--- header.php
+++ header.php
@@ -1,8 +1,4 @@
 	<div id="IDPanel"> <?php echo ucfirst(strtolower(htmlentities($_SESSION['user']['username'], ENT_QUOTES, "UTF-8"))); ?> 
-		<script>
-			var p = $('#content').position();
-			alert(p.top);
-		</script>
 		<li><a href="https://docs.google.com/spreadsheet/ccc?key=0Ar405A90uLjQdHpBU210ZmVoNXVZZ1lMZzZtYmg2NkE#gid=18">Tracking Sheet</a></li>
 		<li><a href="https://adfs.strode-college.ac.uk/CookieAuth.dll?GetLogon?curl=Z2FadfsZ2FlsZ2FZ3FwaZ3Dwsignin1.0Z26wtrealmZ3DurnZ3AfederationZ3AMicrosoftOnlineZ26wctxZ3DwaZ253Dwsignin1.0Z2526rpsnvZ253D2Z2526ctZ253D1370532248Z2526rverZ253D6.1.6206.0Z2526wpZ253DMBI_KEYZ2526wreplyZ253DhttpsZ3AZ25252FZ25252Famxprd0112.outlook.comZ25252FowaZ25252FZ2526idZ253D260563Z2526whrZ253Dstudent.strode-college.ac.ukZ2526CBCXTZ253Dout&reason=0&formdir=6">Email</a></li>
 		<li><a href="http://elib.strode-college.ac.uk">Other Links</a></li>
