02901365ced8f20e5b82f01a735c0586cd4adf50
Jamie Hankins <jamiehankin@gmail.com>
2013-06-06 17:56:51 +0100

    debug

--- update.php
+++ update.php
@@ -4,7 +4,7 @@
 	 *
 	 * Used for automatically deploying websites via github or bitbucket, more deets here:
 	 *
-	 *WORK		https://gist.github.com/1809044
+	 *		https://gist.github.com/1809044
 	 */
 
 	// The commands
