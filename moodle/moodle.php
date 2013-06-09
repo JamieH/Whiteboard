a8f81f8ec25288ef1e9acb739cf99aba9779b201
Jamie Hankins <jamiehankin@gmail.com>
2013-06-09 16:36:40 +0100

    oops

--- moodle/moodle.php
+++ moodle/moodle.php
@@ -131,7 +131,7 @@ function getPassword($username, $db)
             //$tagName = $a->tagName;
             //$childNodes = $a->childNodes;
             print pq($a)->attr('href');
-            print "<br/>"
+            print "<br/>";
     }
 
     }
