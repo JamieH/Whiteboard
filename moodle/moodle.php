1252cff02b58845cb0ffb28c798ecf8710c3680d
Jamie Hankins <jamiehankin@gmail.com>
2013-06-09 15:47:45 +0100

    rot13 away

--- moodle/moodle.php
+++ moodle/moodle.php
@@ -98,23 +98,13 @@ function getSalt($user, $db)
     } 
 }
 
-function encrypt($str, $key)
-{
-    $block = mcrypt_get_block_size('des', 'ecb');
-    $pad = $block - (strlen($str) % $block);
-    $str .= str_repeat(chr($pad), $pad);
+function rot13encrypt ($str) {
+    return str_rot13(base64_encode($str));
+    }
 
-    return mcrypt_encrypt(MCRYPT_DES, $key, $str, MCRYPT_MODE_ECB);
-}
-
-function decrypt($str, $key)
-{   
-    $str = mcrypt_decrypt(MCRYPT_DES, $key, $str, MCRYPT_MODE_ECB);
-
-    $block = mcrypt_get_block_size('des', 'ecb');
-    $pad = ord($str[($len = strlen($str)) - 1]);
-    return substr($str, 0, strlen($str) - $pad);
-}
+function rot13decrypt ($str) {
+    return base64_decode(str_rot13($str));
+    }
 
 function authwithMoodle($username, $authurl, $db)
 {
