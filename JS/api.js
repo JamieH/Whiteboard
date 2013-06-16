d611f509f0292aa46a5377e8f86af6e4af8f0499
Matthew James <Quacky2200@hotmail.com>
2013-06-17 00:17:17 +0100

    trying into container 2

--- JS/api.js
+++ JS/api.js
@@ -9,7 +9,7 @@ function loopUnits(){
 $.get('moodle/api.php',{action: "listunits"}).done(function(data) {
             data = JSON.parse(data);
             $.each(data, function (index, item) {
-            	$(".container").append('<div class="btn-group btn-block"><a class="btn btn-info btn-large btn-block btn-primary dropdown-toggle" data-toggle="dropdown" href="#">' + item + '<span class="caret"></span></a><ul class="dropdown-menu"><li><a href="#">Get Assignments</a></li><li><a href="#">View Resources</a></li><li><a href="#">Get Feedback</a></li></ul></div>');
+            	$(".container:eq(1)").append('<div class="btn-group btn-block"><a class="btn btn-info btn-large btn-block btn-primary dropdown-toggle" data-toggle="dropdown" href="#">' + item + '<span class="caret"></span></a><ul class="dropdown-menu"><li><a href="#">Get Assignments</a></li><li><a href="#">View Resources</a></li><li><a href="#">Get Feedback</a></li></ul></div>');
                 //console.log(item);
             });
 
