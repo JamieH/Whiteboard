63c49eed7e4686161b0d7d4150cf87b07032b09c
Matthew James <Quacky2200@hotmail.com>
2013-06-17 00:37:14 +0100

    github glitch fixed…i hope

--- JS/api.js
+++ JS/api.js
@@ -9,12 +9,9 @@ function loopUnits(){
 $.get('moodle/api.php',{action: "listunits"}).done(function(data) {
             data = JSON.parse(data);
             $.each(data, function (index, item) {
-<<<<<<< HEAD
             	$(".container:eq(1)").append('<div class="btn-group btn-block"><a class="btn btn-info btn-large btn-block btn-primary dropdown-toggle" data-toggle="dropdown" href="#">' + item + '<span class="caret"></span></a><ul class="dropdown-menu"><li onclick="removeall()">Get Assignments</li><li><a href="#">View Resources</a></li><li><a href="#">Get Feedback</a></li></ul></div>');
                 //console.log(item);
-=======
             	addUnit(item);
->>>>>>> 765008dfc40fbbb4e742c0db7ddfdc0bce15ad60
             });
 
 });
@@ -33,8 +30,6 @@ function removeall(){
 			$('div').remove();
 		}
 	}
-
-<<<<<<< HEAD
 }
 function addUnit(){
 
