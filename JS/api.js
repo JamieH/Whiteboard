86510bb368f51ddbf11672d60bf1680cfe601c3a
Matthew James <Quacky2200@hotmail.com>
2013-06-17 00:34:30 +0100

    fix remove assign.

--- JS/api.js
+++ JS/api.js
@@ -10,7 +10,7 @@ $.get('moodle/api.php',{action: "listunits"}).done(function(data) {
             data = JSON.parse(data);
             $.each(data, function (index, item) {
 <<<<<<< HEAD
-            	$(".container:eq(1)").append('<div class="btn-group btn-block"><a class="btn btn-info btn-large btn-block btn-primary dropdown-toggle" data-toggle="dropdown" href="#">' + item + '<span class="caret"></span></a><ul class="dropdown-menu"><li><a onclick="removeall()" href="#">Get Assignments</a></li><li><a href="#">View Resources</a></li><li><a href="#">Get Feedback</a></li></ul></div>');
+            	$(".container:eq(1)").append('<div class="btn-group btn-block"><a class="btn btn-info btn-large btn-block btn-primary dropdown-toggle" data-toggle="dropdown" href="#">' + item + '<span class="caret"></span></a><ul class="dropdown-menu"><li onclick="removeall()">Get Assignments</li><li><a href="#">View Resources</a></li><li><a href="#">Get Feedback</a></li></ul></div>');
                 //console.log(item);
 =======
             	addUnit(item);
